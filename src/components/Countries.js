ab7e86ae406cc42a167708907728edd97b07198c
ronttizz <toni.nissinen@gmail.com>
2022-08-15 14:47:24 +0300

    countries listed

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,7 +1,39 @@
 import CountryCard from "./CountryCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Countries = () => {
-  return <CountryCard country="test country" />;
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .catch((error) => console.log(error))
+      .then((res) => {
+        console.log(res.data);
+        setCountries(res.data);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="countrylisting">
+      {!loading ? (
+        countries.map((country) => {
+          return (
+            <CountryCard
+              country={country.name?.common}
+              key={country.name?.common}
+              {...country}
+            />
+          );
+        })
+      ) : (
+        <p className="parag">Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default Countries;
