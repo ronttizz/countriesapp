182cb8dffd9ad8cc01dd8c9e2606afbe5f4da8f5
ronttizz <toni.nissinen@gmail.com>
2022-08-15 13:54:20 +0300

    Layout setup

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,12 +1,21 @@
 const CountryCard = ({ country }) => {
   return (
     <div className="countrybox">
-      <h1 className="commonname">{country}</h1>
-      <p className="officialname">{country}</p>
+      <div className="names">
+        <h1 className="commonname">{country}</h1>
+        <p className="officialname">{country}</p>
+      </div>
       <div className="countryinfo">
-        <p className="info">Currency</p>
-        <p className="info">Language(s)</p>
-        <p className="info">Population</p>
+        <div className="infoblock">
+          <p className="info">Currency</p>
+          <p className="info">Euro</p>
+        </div>
+        <div className="infoblock">
+          <p className="info">Language(s)</p>
+        </div>
+        <div className="infoblock">
+          <p className="info">Population</p>
+        </div>
       </div>
     </div>
   );
