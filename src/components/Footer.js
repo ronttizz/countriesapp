b7309f0f7609649a4e5d8e106efa877c96d05179
ronttizz <toni.nissinen@gmail.com>
2022-08-15 12:17:30 +0300

    classname added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <small>
-        <p>This will be the copyrights etc</p>
+        <p className="copy">This will be the copyrights etc</p>
       </small>
     </footer>
   );
