7df81817873342b2cafdc97890fe06fa5ac9591a
ronttizz <toni.nissinen@gmail.com>
2022-08-17 12:35:54 +0300

    Flag added

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,10 +1,12 @@
-const CountryCard = ({ country }) => {
+const CountryCard = ({ country, flags }) => {
   return (
     <div className="countrybox">
       <div className="names">
         <h1 className="commonname">{country}</h1>
         <p className="officialname">{country}</p>
       </div>
+      <img src={flags.svg} className="flag" />
+
       <div className="countryinfo">
         <div className="infoblock">
           <p className="info">Currency</p>
