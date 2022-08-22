aba30fc03a528040ba25b4ac6e53c8577d3424dc
ronttizz <toni.nissinen@gmail.com>
2022-08-22 11:01:03 +0300

    Currencies and languages added

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,4 +1,4 @@
-const CountryCard = ({ country, flags }) => {
+const CountryCard = ({ country, flags, currencies, languages }) => {
   return (
     <div className="countrybox">
       <div className="names">
@@ -10,10 +10,19 @@ const CountryCard = ({ country, flags }) => {
       <div className="countryinfo">
         <div className="infoblock">
           <p className="info">Currency</p>
-          <p className="info">Euro</p>
+          <p className="info">
+            {Object.values(currencies || {}).map((currency, i) => (
+              <span key={i}>{(i ? ", " : "") + currency.name}</span>
+            ))}
+          </p>
         </div>
         <div className="infoblock">
           <p className="info">Language(s)</p>
+          <p className="info">
+            {Object.values(languages || {}).map((language, i) => (
+              <span key={i}>{(i ? ", " : "") + language}</span>
+            ))}
+          </p>
         </div>
         <div className="infoblock">
           <p className="info">Population</p>
