464def2bd2731313a9f894ff8bcf4d8cbca01153
ronttizz <toni.nissinen@gmail.com>
2022-08-23 12:20:09 +0300

    Some basic layout for single country page

--- src/components/CountryPage.js
+++ src/components/CountryPage.js
@@ -2,11 +2,12 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Spinner from "react-bootstrap/Spinner";
 import Axios from "axios";
+import millify from "millify";
 
 const CountryPage = () => {
   const params = useParams();
 
-  const [country, setCountry] = useState([]);
+  const [country, setCountry] = useState();
   const [flag, setFlag] = useState(
     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/No_flag.svg/225px-No_flag.svg.png?20220314051100"
   );
@@ -28,7 +29,25 @@ const CountryPage = () => {
 
   return !loading ? (
     <div className="country">
-      <p>This will have information about the country</p>
+      <div className="countryname">
+        <h2>{country[0]?.name?.common}</h2>
+        <h4>{country[0]?.name?.official}</h4>
+      </div>
+      <div className="countryinfo">
+        <div className="leftpanel">
+          <img
+            src={flag}
+            alt={country[0]?.name?.official + " flag"}
+            className="bigflag"
+          />
+        </div>
+        <div className="rightpanel">
+          <ul>
+            <li>{millify(country[0]?.population)}</li>
+            <li>{country[0]?.capital}</li>
+          </ul>
+        </div>
+      </div>
     </div>
   ) : (
     <Spinner animation="border" role="status" variant="light" />
