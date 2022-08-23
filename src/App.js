c16dc7055812ca364be05b1e2651025b343425e7
ronttizz <toni.nissinen@gmail.com>
2022-08-23 09:59:21 +0300

    Removed unused useParams

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, useParams, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./components/Home";
 import Countries from "./components/Countries";
