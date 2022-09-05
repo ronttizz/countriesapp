a76ef0291366947d568664d92bb8ae71e78ca74f
ronttizz <toni.nissinen@gmail.com>
2022-09-05 11:12:12 +0300

    countriesSlice imported

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import countriesSlice from "../features/countries/countriesSlice";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    countries: countriesSlice,
+  },
 });
