7f748f2883b105114042374939d0b682f6ad377c
ronttizz <toni.nissinen@gmail.com>
2022-09-12 13:24:56 +0300

    console log removed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   const itemCount = useSelector((state) => state.countries.favourites.length);
-  console.log(itemCount);
+
   return (
     <nav className="nav">
       <ul>
