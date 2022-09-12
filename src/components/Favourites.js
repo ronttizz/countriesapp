6d99157f0f26d6203066c98ccf1437eabd80881d
ronttizz <toni.nissinen@gmail.com>
2022-09-12 13:06:27 +0300

    Favourite countries displayed

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,7 +1,36 @@
+import { Spinner } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import CountryCard from "./CountryCard";
+import Search from "./Search";
+
 const Favourites = () => {
+  const favourites = useSelector((state) => state.countries.favourites);
+  const loading = useSelector((state) => state.countries.isLoading);
+  const searchTerm = useSelector((state) => state.countries.search);
+
   return (
     <div>
-      <h1>This will have list of favourite countries</h1>
+      <Search />
+      <div className="countrylisting">
+        {!loading ? (
+          favourites
+            .filter((country) => {
+              return (
+                country?.name?.common
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase().trim()) ||
+                country?.name?.official
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase().trim())
+              );
+            })
+            .map((country, i) => {
+              return <CountryCard key={i} {...country} country={country} />;
+            })
+        ) : (
+          <Spinner animation="border" role="status" variant="light" />
+        )}
+      </div>
     </div>
   );
 };
