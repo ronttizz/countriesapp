bb0480e57ec468c09db67017b02fb74028fbed8c
ronttizz <toni.nissinen@gmail.com>
2022-09-19 15:00:00 +0300

    back to top button styled

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import CountryPage from "./components/CountryPage";
 import Favourites from "./components/Favourites";
 import { useDispatch, useSelector } from "react-redux";
 import { saveFavourites } from "./features/countries/countriesSlice";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [scrolled, setScrolled] = useState(false);
   const dispatch = useDispatch();
   const favorites = useSelector((state) => state.countries.favourites);
 
@@ -17,17 +18,37 @@ function App() {
     saveFavourites(favorites);
   }, [dispatch, favorites]);
 
+  window.addEventListener("scroll", () => {
+    window.scrollY > 300 ? setScrolled(true) : setScrolled(false);
+  });
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="countries" element={<Countries />} />
-          <Route path="countries/:name" element={<CountryPage />} />
-          <Route path="favourites" element={<Favourites />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="countries" element={<Countries />} />
+            <Route path="countries/:name" element={<CountryPage />} />
+            <Route path="favourites" element={<Favourites />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {scrolled ? (
+        <div className="scroll">
+          <i className="bi bi-skip-backward-circle" onClick={() => scrollToTop()}></i>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
   );
 }
 
